3998fadfdc18dda8b1535f4cb50d2aefcab25f21
mhtkmr54 <mhtkmr54@gmail.com>
2015-08-08 08:37:56 +0530

    as_success.php

--- Siddhartha/php/businessHomepage.php
+++ Siddhartha/php/businessHomepage.php
@@ -1,3 +1,74 @@
+ <?php echo $txnid ?> <?php echo $orderID ?>
+ 
+ <?php
+                        // Merchant key here as provided by Payu
+                        $MERCHANT_KEY = "IYjt2G";
+                        
+                        // Merchant Salt as provided by Payu
+                        $SALT = "lkXTUXtR";
+                        
+                        // End point - change to https://secure.payu.in for LIVE mode
+                        $PAYU_BASE_URL = "https://secure.payu.in";
+                        
+                        $action = '';
+                        
+                        $posted = array();
+                        if(!empty($_POST)) {
+                            //print_r($_POST);
+                          foreach($_POST as $key => $value) {    
+                            $posted[$key] = $value; 
+                            
+                          }
+                        }
+                        
+                        $formError = 0;
+                        
+                        if(empty($posted['txnid'])) {
+                          // Generate random transaction id
+                          $txnid = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
+                        } else {
+                          $txnid = $posted['txnid'];
+                        }
+                        $hash = '';
+                        // Hash Sequence
+                        $hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
+                        if(empty($posted['hash']) && sizeof($posted) > 0) {
+                          if(
+                                  empty($posted['key'])
+                                  || empty($posted['txnid'])
+                                  || empty($posted['amount'])
+                                  || empty($posted['firstname'])
+                                  || empty($posted['email'])
+                                  || empty($posted['phone'])
+                                  || empty($posted['productinfo'])
+                                  || empty($posted['surl'])
+                                  || empty($posted['furl'])
+                                  || empty($posted['service_provider'])
+                          ) {
+                            $formError = 1;
+                          } else {
+                            //$posted['productinfo'] = json_encode(json_decode('[{"name":"tutionfee","description":"","value":"500","isRequired":"false"},{"name":"developmentfee","description":"monthly tution fee","value":"1500","isRequired":"false"}]'));
+                            $hashVarsSeq = explode('|', $hashSequence);
+                            $hash_string = '';  
+                            foreach($hashVarsSeq as $hash_var) {
+                              $hash_string .= isset($posted[$hash_var]) ? $posted[$hash_var] : '';
+                              $hash_string .= '|';
+                            }
+                        
+                            $hash_string .= $SALT;
+                        
+                        
+                            $hash = strtolower(hash('sha512', $hash_string));
+                            $action = $PAYU_BASE_URL . '/_payment';
+                          }
+                        } elseif(!empty($posted['hash'])) {
+                          $hash = $posted['hash'];
+                          $action = $PAYU_BASE_URL . '/_payment';
+                        }
+                        ?>
+                        
+                       
+
 <?php
 session_start();
 if (!isset($_SESSION['username']) || empty($_SESSION['username']))
@@ -137,14 +208,14 @@ $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
 if (!$link)
 {
-	die('Could not connect: ' . mysql_error());
+    die('Could not connect: ' . mysql_error());
 }
 
 $db_selected = mysql_select_db(DB_NAME, $link);
 
 if (!$db_selected)
 {
-	die('Ca\'t use ' . DB_NAME . ': ' . mysql_error());
+    die('Ca\'t use ' . DB_NAME . ': ' . mysql_error());
 }
 
 
@@ -153,12 +224,14 @@ if (!$db_selected)
 
 
 $user =$_SESSION['username'];
+
     
     
 
 
 $date=date("Y-m-d H:i:s");
 $orderID=rand(10000000,99999999);
+$orderID = $txnid;
 
 $sql ="INSERT INTO businesshomepage (email, txnid, orders, quality, noOfPosts, deliveryTime, topic, industry_of_experience, goal, style_of_writing, sample_blog, point_of_view, blog_structure, target_audience, key_points, things_to_avoid, keywords, special_instructions, dateOrder) VALUES ('$user', '$orderID', '$dataEntry3', '$quality', '$dataEntry1', '$dataEntry2', '$topic', '$industry', '$goal', '$styleOfWriting', '$sampleBlog', '$pointOfView', '$blogStructure', '$targetAudience', '$keyPoints', '$avoid', '$keywords', '$specialInstructions', '$date')";
 
@@ -194,74 +267,11 @@ $name= $con3->firstName." ".$con3->lastName;
 
 ?>
 
- <?php
-                        // Merchant key here as provided by Payu
-                        $MERCHANT_KEY = "IYjt2G";
-                        
-                        // Merchant Salt as provided by Payu
-                        $SALT = "lkXTUXtR";
-                        
-                        // End point - change to https://secure.payu.in for LIVE mode
-                        $PAYU_BASE_URL = "https://secure.payu.in";
-                        
-                        $action = '';
-                        
-                        $posted = array();
-                        if(!empty($_POST)) {
-                            //print_r($_POST);
-                          foreach($_POST as $key => $value) {    
-                            $posted[$key] = $value; 
-                            
-                          }
-                        }
-                        
-                        $formError = 0;
-                        
-                        if(empty($posted['txnid'])) {
-                          // Generate random transaction id
-                          $txnid = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
-                        } else {
-                          $txnid = $posted['txnid'];
-                        }
-                        $hash = '';
-                        // Hash Sequence
-                        $hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
-                        if(empty($posted['hash']) && sizeof($posted) > 0) {
-                          if(
-                                  empty($posted['key'])
-                                  || empty($posted['txnid'])
-                                  || empty($posted['amount'])
-                                  || empty($posted['firstname'])
-                                  || empty($posted['email'])
-                                  || empty($posted['phone'])
-                                  || empty($posted['productinfo'])
-                                  || empty($posted['surl'])
-                                  || empty($posted['furl'])
-                                  || empty($posted['service_provider'])
-                          ) {
-                            $formError = 1;
-                          } else {
-                            //$posted['productinfo'] = json_encode(json_decode('[{"name":"tutionfee","description":"","value":"500","isRequired":"false"},{"name":"developmentfee","description":"monthly tution fee","value":"1500","isRequired":"false"}]'));
-                            $hashVarsSeq = explode('|', $hashSequence);
-                            $hash_string = '';  
-                            foreach($hashVarsSeq as $hash_var) {
-                              $hash_string .= isset($posted[$hash_var]) ? $posted[$hash_var] : '';
-                              $hash_string .= '|';
-                            }
-                        
-                            $hash_string .= $SALT;
-                        
-                        
-                            $hash = strtolower(hash('sha512', $hash_string));
-                            $action = $PAYU_BASE_URL . '/_payment';
-                          }
-                        } elseif(!empty($posted['hash'])) {
-                          $hash = $posted['hash'];
-                          $action = $PAYU_BASE_URL . '/_payment';
-                        }
-                        ?>
 
-<?php $orderID = $txnid  ?>
+
+
+<?php // echo $orderID 
+?>
 <html>
     <head>
 
@@ -510,16 +520,16 @@ $name= $con3->firstName." ".$con3->lastName;
                             <h2>Industry in which the writer may have experience</h2>
                             <select required id='industryid' name="industry" >
                                 <option value="">--select your area--</option>
-                                <option value="Sports & Fitness">Sports & Fitness</option>
+                                <option value="Sports and Fitness">Sports & Fitness</option>
                                 <option value="Business">Business</option>
-                                <option value="Art & Design">Art & Design</option>
-                                <option value="Food & Beverage">Food & Beverage</option>
+                                <option value="Art and Design">Art & Design</option>
+                                <option value="Food and Beverage">Food & Beverage</option>
                                 <option value="Entertainment">Entertainment</option>
-                                <option value="Healthcare">Healthcare</option>
-                                <option value="Journalism & Publishing">Journalism & Publishing</option>
-                                <option value="Lifestyle & Travel">Lifestyle & Travel</option>
+                                <option value="Healthcare and Sciences">Healthcare and Sciences</option>
+                                <option value="Publishing and Journalism">Journalism & Publishing</option>
+                                <option value="Lifestyle and Travel">Lifestyle & Travel</option>
                                 <option value="Education">Education</option>
-                                <option value="Software Technology">Software Technology</option>
+                                <option value="Software and Technology">Software and Technology</option>
                             </select>
                             
                             <br/>
@@ -602,16 +612,16 @@ $name= $con3->firstName." ".$con3->lastName;
                             </script>
                           
     
-				    <script type="text/javascript">
+                    <script type="text/javascript">
     
-					    function reply_click(element)
-					    {
-					    document.getElementById('product_name').value = element.getAttribute('data-product-name');
-					    }    
-					    
+                        function reply_click(element)
+                        {
+                        document.getElementById('product_name').value = element.getAttribute('data-product-name');
+                        }    
+                        
 
-				    
-			   </script>
+                    
+               </script>
                           
                           
                             <h2>Enter your contact detail and confirm order</h2>
@@ -631,8 +641,9 @@ $name= $con3->firstName." ".$con3->lastName;
                                   <td><b>Mandatory Parameters</b></td>
                                 </tr>
                                 <tr>
+                                     
                                   <td>Amount: Rs.<?php echo $quality; ?></td>
-                                  <td><input type="hidden" name="amount" id="product_name" value="<?php echo (empty($posted['amount'])) ? $quality: $posted['amount'] ?>" /></td>
+                                  <td><input type="hidden" name="amount" id="product_name" value="<?php echo (empty($posted['amount'])) ? '1': $posted['amount'] ?>" /></td>
                                   <td> &nbsp&nbsp Name:  <?php echo $name; ?> </td>
                                   <td><input type="hidden" name="firstname" id="firstname" value="<?php echo (empty($posted['firstname'])) ? $name : $posted['firstname']; ?>" /></td>
                                 </tr>
@@ -648,7 +659,7 @@ $name= $con3->firstName." ".$con3->lastName;
                                 </tr>
                                 <tr>
                                   
-                                  <td colspan="3"><input type="hidden" name="surl" value="<?php echo (empty($posted['surl'])) ? 'http://whitepanda.in/successs.php' : $posted['surl'] ?>" size="64" /></td>
+                                  <td colspan="3"><input type="hidden" name="surl" value="<?php echo (empty($posted['surl'])) ? 'http://whitepanda.in/success.php' : $posted['surl'] ?>" size="64" /></td>
                                 </tr>
                                 <tr>
                                   
